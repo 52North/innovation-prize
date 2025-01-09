6036d6753b7f66275622b83c2e4c7b5e07affb8f
Henning Bredel <h.bredel@52north.org>
2025-01-09 13:27:37 +0100

    Configure sdgs api key

--- search-app/server/config/config.py
+++ search-app/server/config/config.py
@@ -9,6 +9,7 @@ class Config:
         self.load_config_file()
         
         # optional overrides from env
+        self.sdsa_api_key = os.getenv("SDSA_API_KEY", self.sdsa_api_key)
         self.openai_api_key = os.getenv("OPENAI_API_KEY", self.openai_api_key)
         self.tavily_api_key = os.getenv("TAVILY_API_KEY", self.tavily_api_key)
         self.groq_api_key = os.getenv("GROQ_API_KEY", self.groq_api_key)
